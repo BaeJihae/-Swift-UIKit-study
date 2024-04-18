1a73ab9f894e8da0b97a7a42c5a93015260f05ed
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-18 12:53:42 +0900

    [UIPageControlPractice]

---
+++ UIPageControlPractice/UIPageControlPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPageControlPractice
+//
+//  Created by 배지해 on 4/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
